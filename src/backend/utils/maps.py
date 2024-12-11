b223f8e1081d7459433bf4307c157ffe60293342
Luiz Hoffmann <luiz.hoffmann@br.ey.com>
2024-12-11 14:07:51 -0300

    Adicionando funções para padronizar telefone e encontrar incoerencia nos grupos

--- src/backend/utils/maps.py
+++ src/backend/utils/maps.py
@@ -1,5 +1,7 @@
 import re
 import tiktoken
+import json
+from src.backend.utils.logger_config import logger
 
 # Função para contar tokens de um prompt
 def contar_tokens(prompt: str, modelo="cl100k_base"):
@@ -9,7 +11,7 @@ def contar_tokens(prompt: str, modelo="cl100k_base"):
     return len(tokens)
 
 # Função para limitar o número de tokens
-def limitar_tokens(prompt: str, max_tokens=8192, modelo="cl100k_base"):
+def limitar_tokens(prompt: str, max_tokens=8000, modelo="cl100k_base"):
     tokens = contar_tokens(prompt, modelo)
     if tokens > max_tokens:
         print(f"Prompt contém {tokens} tokens, que é maior que o limite de {max_tokens} tokens.")
@@ -24,21 +26,37 @@ def limitar_tokens(prompt: str, max_tokens=8192, modelo="cl100k_base"):
 # Função principal para extrair informações
 def extract_information_from_page(conversation, page_text):
     # Criação do prompt
+
     prompt_string = (
-        f"""
-        Extraia as seguintes informações do texto fornecido, se disponíveis:
+    f"""
+    Você é um agente de inteligência artificial especializado em extrair informaçÕes e redigir resumos jurídicos de maneira precisa e objetiva.  
+    Você recebeu um texto referente à uma página de uma reclamação trabalhista.
+    O texto da página está abaixo entre ***.
+    Extraia as informações abaixo do texto fornecido.
+    Seja direto, objetivo e responda apenas com os dados solicitados e no formato especificado. 
+    Se alguma informação não for encontrada, responda "Não identificado".
 
-        1. **Processo:** Apenas o número.
-        2. **Autor:** Apenas o autor
-        3. **Réu:** Apenas o réu
-        4. **Resumo:** Resumo objetivo do que tem na página. 
-        5. **Pedidos:** O que o autor busca.
-        6. **Decisões:** Andamento atual e movimentações importantes.
+    ### Formato da Resposta ###
+    Retorne exclusivamente uma resposta em JSON no seguinte formato:
 
-        Se algo não estiver claro ou ausente, indique "Não identificado". Seja objetivo e direto.
+    {{
+    "Processo" :  "<Número da reclamação trabalhista (ex.: "0010005-64.2021.5.03.0187")>",
+    "Autor" : "<Nome do autor do processo (também chamado de adverso, reclamante ou ex-empregado)>",
+    "Salário" : "<Valor do salário ou remuneração do autor (ex.: "R$ 2.500,00")>",
+    "Tempo" : "<Tempo de serviço do autor na empresa (ex.: "5 anos e 3 meses")>",
+    "Resumo" : "<Resumo objetivo do conteúdo da página. Comece sempre com "O documento diz...">",
+    "Valor" : "<Valor da causa informado no processo, costuma ser o maior valor escrito no documento (ex.: "R$ 500.000,00")>",
+    "Objetos" : "<Objetos da reclamação e seus valores, no formato "<Objeto>" - "<Valor>" (ex.: "Hora extra" - "R$ 2.000,00", "Férias" - "R$ 3.500,00", "FGTS" - "Não identificado")>"
+    }}
 
-        Texto da página: {page_text}
-        """
+    ### Regras Adicionais ###
+    - Não inclua frases desnecessárias que indiquem a origem das informações.
+    - Para o resumo, construa o texto de forma fluida e seja objetivo e mantenha um tom formal e técnico.
+    
+    Texto da página ***. {page_text} ***.
+
+    Resposta em JSON:        
+    """
     )
     
     # Limitar o prompt para não ultrapassar o número máximo de tokens
@@ -46,47 +64,119 @@ def extract_information_from_page(conversation, page_text):
 
     # Chamada para o modelo de conversa
     response = conversation.run(prompt_string_limited)  # Use .run() para strings diretas
-    return response  # Retorna a resposta do modelo
+    print("response: ",response)
+    try:
+        # Limpar espaços e quebras de linha
+        response_cleaned = response.strip()
+
+        response_cleaned = response_cleaned.replace('\\"', "'")
+
+        # Verificar se é JSON válido
+        if response_cleaned.startswith('{') and response_cleaned.endswith('}'):
+            response_json = json.loads(response_cleaned)
+        else:
+            logger.error("Formato inválido, o texto não é JSON.")
+
+            response_json =  {
+                "Processo" : "Não identificado",
+                "Autor" : "Não identificado",
+                "Salário" : "Não identificado",
+                "Tempo" : "Não identificado",
+                "Resumo" : "",
+                "Valor" : "Não identificado",
+                "Objetos" : "Não identificado"
+            }
+            return response_json
+        
+    except json.JSONDecodeError as e:
+        logger.error(f"Erro ao decodificar JSON: {e}")
+        return {"resumo": "Erro ao resumir: falha ao decodificar JSON"}
+            
+    return response_json  # Retorna a resposta do modelo
 
 # Função para atualizar o dicionário consolidado, sem duplicar informações
-def update_all_pages_data(data_dict, extracted_text):
+def update_all_pages_data(data_dict, extracted_dict):
     """
     Atualiza o dicionário consolidado com os dados extraídos de uma página usando regex.
     """
-    # Limpar os espaços em branco no começo e final do texto
-    extracted_text = extracted_text.strip()
+    
+    data_dict["Processo"].add(extracted_dict["Processo"])
+    
+    data_dict["Autor"].add(extracted_dict["Autor"])
 
-    # Definindo os padrões regex para cada seção, agora considerando o formato que você forneceu
-    processo_pattern = r"1\.\sProcesso\:\s*(.*?)(?=\n2\.|$)"
-    autor_pattern = r"2\.\sAutor\:\s*(.*?)(?=\n3\.|$)"
-    reu_pattern = r"3\.\sRéu\:\s*(.*?)(?=\n4\.|$)"
-    resumo_pattern = r"4\.\sResumo\:\s*(.*?)(?=\n5\.|$)"
-    pedidos_pattern = r"5\.\sPedidos\:\s*(.*?)(?=\n6\.|$)"
-    decisoes_pattern = r"6\.\sDecisões\:\s*(.*?)(?=\n|$)"
+    data_dict["Salário"].add(extracted_dict["Salário"])
 
-    # Usar regex para encontrar os dados de cada seção
-    processo_match = re.search(processo_pattern, extracted_text, re.DOTALL)
-    autor_match = re.search(autor_pattern, extracted_text, re.DOTALL)
-    reu_match = re.search(reu_pattern, extracted_text, re.DOTALL)
-    resumo_match = re.search(resumo_pattern, extracted_text, re.DOTALL)
-    pedidos_match = re.search(pedidos_pattern, extracted_text, re.DOTALL)
-    decisoes_match = re.search(decisoes_pattern, extracted_text, re.DOTALL)
+    data_dict["Tempo"].add(extracted_dict["Tempo"])
 
-    # Atualizar o dicionário com os dados extraídos, mas apenas se não estiverem já definidos
-    if processo_match and data_dict["Processo"] == "Não identificado":
-        data_dict["Processo"] = processo_match.group(1).strip()
-        
-    if autor_match and data_dict["Autor"] == "Não identificado":
-        data_dict["Autor"] = autor_match.group(1).strip()
-        
-    if reu_match and data_dict["Réu"] == "Não identificado":
-        data_dict["Réu"] = reu_match.group(1).strip()
+    data_dict["Valor"].add(extracted_dict["Valor"])
 
-    if resumo_match:
-        data_dict["Resumo"].append(resumo_match.group(1).strip())
+    data_dict["Objetos"].add(extracted_dict["Objetos"])
+    
+    data_dict["Resumo"].append(extracted_dict["Resumo"])
 
-    if pedidos_match:
-        data_dict["Pedidos"].append(pedidos_match.group(1).strip())
 
-    if decisoes_match:
-        data_dict["Decisões"].append(decisoes_match.group(1).strip())
+def padronizar_telefone(telefone):
+    telefone = ''.join(filter(str.isdigit, str(telefone)))
+    
+    if len(telefone) == 10:
+        return f"({telefone[:2]}) {telefone[2:6]}-{telefone[6:]}"
+    
+    elif len(telefone) == 8:
+        return f"{telefone[:4]}-{telefone[4:]}"
+    
+    else:
+        return telefone
+    
+def validar_grupo(conversation,grupo,textos):
+    try:
+        itens_texto = ', '.join(f"- {texto}" for texto in textos)
+
+        prompt = f"""
+            Você é um agente de inteligência artificial trabalhando no setor de compras da CCR.
+            Sua tarefa é analisar uma lista de materiais agrupados em categorias e determinar se todos os materiais estão coerentes com o nome do grupo.
+
+            ### Instruções ###
+            - Considere que o nome do grupo representa uma categoria ampla que pode incluir sinônimos ou itens relacionados.
+            - Analise cuidadosamente se cada material pertence à categoria representada pelo grupo.
+            - Use bom senso para identificar associações plausíveis entre o nome do grupo e os materiais listados. Exemplo:
+            - Grupo: "Ferramentas". Materiais: "Martelo", "Alicate" — **Coerente**.
+            - Grupo: "Ferramentas". Materiais: "Notebook", "Cadeira" — **Incoerente**.
+            - Se o material não parecer pertencer ao grupo, mas for ambíguo, dê o benefício da dúvida e considere como coerente.
+
+            ### Formato da Resposta ###
+            Retorne exclusivamente uma resposta em JSON no seguinte formato:
+            {{
+            "status": "Ok" ou "Incoerente",
+            "materiais_incoerentes": ["Material 1", "Material 2", ...]
+            }}
+            - Se todos os materiais forem coerentes com o grupo, retorne:
+            {{
+                "status": "Ok",
+                "materiais_incoerentes": []
+            }}
+            - Se houver materiais incoerentes, liste apenas os nomes dos materiais incoerentes no campo `materiais_incoerentes`.
+
+            ### Regras Adicionais ###
+            - Não inclua explicações, saudações, despedidas ou informações adicionais.
+            - Não faça suposições desnecessárias. Seja objetivo ao determinar a coerência.
+
+            Grupo: {grupo}
+            Materiais:
+            {itens_texto}
+
+            Resposta em JSON:
+            """
+
+        # prompt_limited = limitar_tokens(prompt)
+        validacao = conversation.run(prompt)
+        conversation.memory.clear()
+
+        # Processa o JSON retornado
+        validacao_json = json.loads(validacao)
+        return validacao_json
+    except json.JSONDecodeError:
+        logger.error(f"Erro ao decodificar JSON para o grupo {grupo}: {validacao}")
+        return {"status": "Erro", "materiais_incoerentes": []}
+    except Exception as e:
+        logger.error(f"Erro ao validar grupo {grupo}: {str(e)}")
+        return {"status": "Erro", "materiais_incoerentes": []}
